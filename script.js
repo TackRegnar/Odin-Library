c052ea7dd61df5ef1b952ff0f190863c6817a6b7
TackRegnar <TackRegnar@protonmail.com>
2022-11-10 21:13:21 -0600

    Very Basic Frame Up

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+
+}
+
+function createBookCard() {
+
+}
